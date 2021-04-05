5275b4fc5668b3e422a439cdce6d447423d82b02
jmiller9991 <jacobmiller9991@gmail.com>
2021-04-05 17:27:55 -0400

    Filler to update

--- DataManip/VideoManip.py
+++ DataManip/VideoManip.py
@@ -9,17 +9,27 @@
 
 import cv2
 import sys
+import os
 
 workingDir = 'C:\\Users\\jdude\\Desktop\\Spring2021\\CS599\\Gameplays'
 
+#This method will get the videos and save them as an array or arrays
 def getAndModifyVideos():
-    print('Test')
+    for x in os.listdir(workingDir):
+        if x.startswith('GP'):
+            dirString = os.path.join(workingDir, x)
+            print('Looking at folder ' + dirString)
+            for files in os.listdir(os.path.join(workingDir, x)):
+                if files.endswith('.mkv'):
+                    print(files)
 
 def main():
     global workingDir
 
     if len(sys.argv) > 2:
         workingDir = sys.argv[1]
+
+    getAndModifyVideos()
 
 
 
