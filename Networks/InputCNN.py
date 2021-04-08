bf004e1ab1f6fb4b631fccfcc773a60572358a76
jmiller9991 <jacobmiller9991@gmail.com>
2021-04-08 17:44:25 -0400

    Created networks folder and started CNN for user input analysis.

--- Networks/InputCNN.py
+++ Networks/InputCNN.py
@@ -8,3 +8,25 @@
 # VideoManip.py and the control input from TextManip.py will be    ###############
 # used as labels.                                                  ###############
 ##################################################################################
+
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras as ks
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Input, Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, LSTM, TimeDistributed
+
+def trainingModel():
+    print('Training model!')
+
+def buildModel(inputShape, classCnt):
+    model = Sequential()
+
+    model.add(Input(shape=(30, 282, 230, 3)))
+    model.add(TimeDistributed(Conv2D(filters=64, kernel_size=3, activation='relu')))
+
+def main():
+    print('Main')
+
+if __name__ == '__main__':
+    main()
