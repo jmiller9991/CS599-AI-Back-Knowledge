a2e9bba0c7cf7efeed1ac2f2e0b4753c29d78a3f
jmiller9991 <jacobmiller9991@gmail.com>
2021-04-08 20:57:52 -0400

    Designed the model, it is currently untested.

--- Networks/InputCNN.py
+++ Networks/InputCNN.py
@@ -19,11 +19,50 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, LSTM, TimeDistributed
 def trainingModel():
     print('Training model!')
 
+#This method builds and compiles a model
 def buildModel(inputShape, classCnt):
+    # INPUT SHAPE
+    # MUST BE (a, b, c, d)
+    # Where: a is number of images entering
+    #        b is length of image
+    #        c is height of image
+    #        d is number of channels in the image
+
+    print('Creating Model...')
     model = Sequential()
 
-    model.add(Input(shape=(30, 282, 230, 3)))
+    print('Developing CNN...')
+    model.add(Input(shape=inputShape))
+    model.add(TimeDistributed(Conv2D(filters=128, kernel_size=6, activation='relu')))
+    model.add(TimeDistributed(MaxPooling2D(3)))
+    model.add(TimeDistributed(Conv2D(filters=128, kernel_size=6, activation='relu')))
+    model.add(TimeDistributed(MaxPooling2D(3)))
+    model.add(TimeDistributed(Conv2D(filters=128, kernel_size=6, activation='relu')))
+    model.add(TimeDistributed(MaxPooling2D(3)))
     model.add(TimeDistributed(Conv2D(filters=64, kernel_size=3, activation='relu')))
+    model.add(TimeDistributed(MaxPooling2D(3)))
+    model.add(TimeDistributed(Conv2D(filters=64, kernel_size=3, activation='relu')))
+    model.add(TimeDistributed(MaxPooling2D(3)))
+    model.add(TimeDistributed(Conv2D(filters=64, kernel_size=3, activation='relu')))
+    model.add(TimeDistributed(MaxPooling2D(3)))
+    model.add(TimeDistributed(Flatten()))
+
+    print('Developing Class Counter')
+    model.add(LSTM(128, return_state=True))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(rate=0.2))
+    model.add(Dense(classCnt, activation='softmax'))
+
+    model.summary()
+
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['binary_accuracy', 'loss'])
+
+    epochs = 200
+    batch_size = 200
+
+    return model, epochs, batch_size
+
+
 
 def main():
     print('Main')
