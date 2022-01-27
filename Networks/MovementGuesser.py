a33b808e30ef4fa2c8004266786ac79da2e101b6
jmiller9991 <jacobmiller9991@gmail.com>
2022-01-26 22:29:54 -0500

    Updated DenseOpticalFlow code and added MovementGuesser.py

--- Networks/MovementGuesser.py
+++ Networks/MovementGuesser.py
@@ -18,3 +18,19 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, LSTM, TimeDistributed
 import os
 import pandas as pd
 import math
+
+workingDir = 'C:\\Users\\jdude\\Desktop\\Spring2021\\CS599\\Gameplays'
+
+def buildTrainingModel(datastrings, inputimages):
+    print('dasdas')
+
+def buildModel(inputShape, classCnt):
+    print('sdasdsdasd')
+
+def main():
+    global workingDir
+
+    print('Main')
+
+if __name__ == '__main__':
+    main()
